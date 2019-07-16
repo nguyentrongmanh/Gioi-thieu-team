4bdd26220c20496af7784ee0edb9fd5103aafe15
manhnguyen0502 <nguyentrongmanh050298@gmail.com>
2019-07-16 15:19:16 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Portfolio from "./components/Home/Portfolio";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Portfolio></Portfolio>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Home/Portfolio/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Portfolio = () => {
+    return (
+        <div>Hello word</div>
+    )
+}
+
+export default Portfolio
